ab84a88b9dd935fbf286385ba00a5124c5e4fb6b
amandadantasds <strangersaturn@gmail.com>
2025-05-08 22:26:19 -0300

    Primeiro Commit

---
+++ app/src/main/java/amandadantasds/com/github/android_lista_de_compras/MainActivity.kt
@@ -0,0 +1,13 @@
+package amandadantasds.com.github.android_lista_de_compras
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
